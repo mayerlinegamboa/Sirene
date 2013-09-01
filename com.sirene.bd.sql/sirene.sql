1f8aba35209e256d055253269c4f02db3bd7eb63
Nelsy Eliana <Nelsy Eliana@NelsyEliana-PC>
2013-08-31 20:21:58 -0500

    Se contribuye con la segunda parte de la base de  datos

--- com.sirene.bd.sql/sirene.sql
+++ com.sirene.bd.sql/sirene.sql
@@ -224,3 +224,222 @@ CREATE TABLE IF NOT EXISTS `tblzona` (
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 01-09-2013 a las 02:42:23
+-- Versión del servidor: 5.5.8
+-- Versión de PHP: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `sirene`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblacudientes`
+--
+
+CREATE TABLE IF NOT EXISTS `tblacudientes` (
+  `secacudientes` int(11) NOT NULL AUTO_INCREMENT,
+  `secestudiante` int(11) NOT NULL,
+  PRIMARY KEY (`secacudientes`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblacudientes`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblbarrio`
+--
+
+CREATE TABLE IF NOT EXISTS `tblbarrio` (
+  `secbarrio` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcionbarrio` varchar(45) NOT NULL,
+  `secciudad` int(11) NOT NULL,
+  PRIMARY KEY (`secbarrio`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblbarrio`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblcargodocente`
+--
+
+CREATE TABLE IF NOT EXISTS `tblcargodocente` (
+  `seccargodocente` int(11) NOT NULL AUTO_INCREMENT,
+  `secpersona` int(11) NOT NULL,
+  `seccargo` int(11) NOT NULL,
+  PRIMARY KEY (`seccargodocente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblcargodocente`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblciudad`
+--
+
+CREATE TABLE IF NOT EXISTS `tblciudad` (
+  `secciudad` int(11) NOT NULL AUTO_INCREMENT,
+  `secbarrio` int(11) NOT NULL,
+  `secdepartamento` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`secciudad`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblciudad`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblcolegioprocedencia`
+--
+
+CREATE TABLE IF NOT EXISTS `tblcolegioprocedencia` (
+  `seccolegioprocedencia` int(11) NOT NULL AUTO_INCREMENT,
+  `secinstituciones` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`seccolegioprocedencia`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblcolegioprocedencia`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbldocumento`
+--
+
+CREATE TABLE IF NOT EXISTS `tbldocumento` (
+  `secdocumento` int(11) NOT NULL AUTO_INCREMENT,
+  `sectipodedocumento` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`secdocumento`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tbldocumento`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblgrado`
+--
+
+CREATE TABLE IF NOT EXISTS `tblgrado` (
+  `secgrado` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`secgrado`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblgrado`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblinstitucion`
+--
+
+CREATE TABLE IF NOT EXISTS `tblinstitucion` (
+  `secinstitucion` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `nit` varchar(45) NOT NULL,
+  `direccion` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`secinstitucion`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblinstitucion`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblpais`
+--
+
+CREATE TABLE IF NOT EXISTS `tblpais` (
+  `secpais` int(11) NOT NULL,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`secpais`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcar la base de datos para la tabla `tblpais`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tblrol`
+--
+
+CREATE TABLE IF NOT EXISTS `tblrol` (
+  `secrol` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`secrol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tblrol`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tbltipodesangre`
+--
+
+CREATE TABLE IF NOT EXISTS `tbltipodesangre` (
+  `sectipodesangre` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(45) NOT NULL,
+  PRIMARY KEY (`sectipodesangre`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Volcar la base de datos para la tabla `tbltipodesangre`
+--
+
